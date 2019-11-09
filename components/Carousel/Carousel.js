858d531d966efe905424670de34bb4f92c657627
Antoinette <create@aplusmedia.info>
2019-11-08 23:45:22 -0500

    attepted carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,35 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector(".carousel-container")
+
+const Carousel () {
+  
+   const carouselDiv = document.createElement("div");
+   carouselDiv.classList.add("carousel");
+
+   const leftButton = document.createElement("div");
+   leftButton.classList.add("left-button");
+
+   const bones = document.createElement("img");
+   bones.src = assets/bones.jpg;
+
+   const fido = document.createElement("img");
+    fido.src =  assets/fido.jpg;
+
+   const max = document.createElement("img");
+   max.src = assets/max.jpg;
+
+   const puppers = document.createElement("img");
+   puppers.src = assets/puppers.jpg
+
+   const sir = document.createElement("img");
+   sir.src = assets/sir.jpg;
+   
+   const rightButton = document.createElement("div");
+   rightButton.classList.add("right-button");
+
+return Carousel;
+
+}
